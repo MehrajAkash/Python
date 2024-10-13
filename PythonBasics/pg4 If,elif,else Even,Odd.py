2b561978868c3db1a0270499119735d8c7e066ae
Md. Mehraj Hossain Akash <mehrajakash81@gmail.com>
2024-10-13 21:06:52 +0600

    random number, if/else

---
+++ PythonBasics/pg4 If,elif,else Even,Odd.py
@@ -0,0 +1,42 @@
+"""
+b = 1
+ while b <= 3:
+     n = int(input('Enter a positive number: '))
+     if n % 2==0:
+         print("%d is even" %n)
+     else:
+         print('{0} is odd'.format(n))
+     b+=1
+
+"""
+from ctypes import c_int
+
+d = -55
+print(abs(d)) #absolute value
+
+n=0
+while n <= 3:
+    number = abs(int(input('Enter a positive number: ')))
+
+    if number%2==0:
+        print('%d Even number' %number)
+    else:
+        print('{} Odd number' .format(number))
+    n+=1
+print('1st part end')
+print('')
+
+a = 1
+while a<=5:
+    num = float(input("Enter a number: "))
+    if num >= 0:
+        if num == 0:
+            print("Zero")
+        else:
+            print("Positive number")
+    else:
+        print('Negative number')
+    a+=1
+
+
+
