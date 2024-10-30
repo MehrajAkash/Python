87f1bb19ebb6624b3e7a376ae59767b021e3f808
Md. Mehraj Hossain Akash <mehrajakash81@gmail.com>
2024-10-30 23:08:22 +0600

    count & len method

---
+++ PythonBasics/Count & len method.py
@@ -0,0 +1,8 @@
+# count method uses for determining how many time a list item exists in a certain list
+mylist = ['apple','banana','mango','banana']
+x = mylist.count("banana")
+print(x) # 2
+
+mylist = [2,5,6,5,3,5]
+print(mylist.count(5)) # 3
+print(len(mylist))  # 6
--- PythonBasics/List/List Copy & Join.py
+++ PythonBasics/List/List Copy & Join.py
@@ -1,4 +1,4 @@
-
+#************* list copying ***************
 thislist = ['apple','banana','cherry']
 mylist = thislist.copy()  # ['apple', 'banana', 'cherry']
 print(mylist)
@@ -7,3 +7,27 @@ thislist = ['animal',4,'people']
 mylist = list(thislist)
 print(mylist)
 
+thislist = ['animal',4,'people']
+list2 = thislist[:]
+print(thislist)
+
+
+
+# ********* join two lists ***************
+list1 = ['a','b','c']
+list2 = [1,2,3]
+list3 = list1 + list2  # ['a', 'b', 'c', 1, 2, 3]
+print(list3)
+
+list1 = ['e','f','g']
+list2 = ['i','j','k']
+
+for x in list2:
+    list1.append(x)
+print(list1)    # ['e', 'f', 'g', 'i', 'j', 'k']
+
+
+list1 = ['e','f','g']
+mylist = list(list1)  # list copying
+mylist.extend(list2)  # list joining
+print(mylist)    # ['e', 'f', 'g', 'i', 'j', 'k']
